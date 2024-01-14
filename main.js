c720e6e3e26b610faa5c323723bca55c84204f52
Fungi <150519355+shroomnet@users.noreply.github.com>
2024-01-14 02:14:18 -0500

    Display match results on screen

--- main.js
+++ main.js
@@ -27,42 +27,35 @@ function getComputerChoice() {
 
 
 // Function to play a single round
-function playRound() {
+function playRound(choice) {
     // Get player choice
-    const playerChoice = prompt("Rock, Paper or Scissors?");
+    const playerChoice = choice;
     // Get the computer's choice
     const computerSelection = getComputerChoice()
     // Initialize the result variable
     let result;
     // Initialize score board variable
     let scoreBoard;
-    // Format the player's input
-    const selection = playerChoice;
-    const selectionCapitalized = selection.charAt(0).toUpperCase() + selection.slice(1);
-    // Check the player's input for errors
-    if (selectionCapitalized !== "Rock" || "Paper" || "Scissors") {
-        result = "Please make a valid selection."
-    }
     // Check for a tie
-    if (selectionCapitalized == computerSelection) {
+    if (playerChoice == computerSelection) {
         result = "It's a tie! Play again.."
         // Check for a winner
-    } else if (selectionCapitalized == "Rock" && computerSelection == "Scissors") {
+    } else if (playerChoice == "Rock" && computerSelection == "Scissors") {
         result = "Rock smashes scissors, you win the round!"
         ++playerScore
-    } else if (selectionCapitalized == "Paper" && computerSelection == "Rock") {
+    } else if (playerChoice == "Paper" && computerSelection == "Rock") {
         result = "Paper covers Rock, you win the round!"
         ++playerScore
-    } else if (selectionCapitalized == "Scissors" && computerSelection == "Paper") {
+    } else if (playerChoice == "Scissors" && computerSelection == "Paper") {
         result = "Scissors slash Paper, you win the round!"
         ++playerScore
-    } else if (computerSelection == "Rock" && selectionCapitalized == "Scissors") {
+    } else if (computerSelection == "Rock" && playerChoice == "Scissors") {
         result = "Rock crushes Scissors, you lose the round.."
         ++computerScore
-    } else if (computerSelection == "Paper" && selectionCapitalized == "Rock") {
+    } else if (computerSelection == "Paper" && playerChoice == "Rock") {
         result = "Paper covers Rock, you lose the round.."
         ++computerScore
-    } else if (computerSelection == "Scissors" && selectionCapitalized == "Paper") {
+    } else if (computerSelection == "Scissors" && playerChoice == "Paper") {
         result = "Scissors cuts Paper, you lose the round.."
         ++computerScore
     }
@@ -85,17 +78,38 @@ function endGame() {
 // Function to play a game
 function game() {
 
-    // Select the buttons
-    const btns = document.querySelectorAll('button')
-
     // Create the game buttons
     const ROCK = document.createElement('button')
+    ROCK.setAttribute('id', 'Rock')
+    ROCK.textContent = 'ROCK'
     const PAPER = document.createElement('button')
+    PAPER.setAttribute('id', 'Paper')
+    PAPER.textContent = 'PAPER'
     const SCISSORS = document.createElement('button')
+    SCISSORS.setAttribute('id', 'Scissors')
+    SCISSORS.textContent = 'SCISSORS'
+    
+    // Select the buttons div
+    const gameBtns = document.querySelector('#gameBtns');
 
-    // For every game button, add an event listener
-    btns.forEach(document.addEventListener('click', playRound))
+    // Select the results div
+    const gameResults = document.querySelector('#gameResults')
+    // Select the result paragraph
+    const displayResult = document.querySelector('#displayResult')
 
+    // Append the game buttons to the buttons div
+    gameBtns.appendChild(ROCK)
+    gameBtns.appendChild(PAPER)
+    gameBtns.appendChild(SCISSORS)
+
+    // Add an event listener to the buttons div
+    gameBtns.addEventListener('click', (e) => {
+        // Get the game button id
+        let choice = e.target.id
+        // When a button is clicked, play a round
+        let roundResult = playRound(choice)
+        displayResult.textContent = roundResult
+    })
 
 }
 
